0e8ceb786786cd1b2cc8aba393b5fa4b29a0050e
xpieemma <7chrispeter@gmail.com>
2026-02-02 14:46:58 -0500

    bring the html ids over | DOM

---
+++ src/script.js
@@ -0,0 +1,18 @@
+
+
+
+const DOM = {
+    themeToggle: document.getElementById('theme-toggle'),
+    searchInput: document.getElementById('search-input'),
+    regionFilter: document.getElementById('region-filter'),
+    countriesGrid: document.getElementById('countries-grid'),
+    homeView: document.getElementById('home-view'),
+    detailView: document.getElementById('detail-view'),
+    countryDetails: document.getElementById('country-details'),
+    backButton: document.getElementById('back-button'),
+    appTitle: document.getElementById('app-title')
+
+};
+
+
+
